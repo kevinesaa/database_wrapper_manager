30209abfbba91f13111425754866bd8c77a33f54
Kevin Esaa <kevin.esaa@gmail.com>
2024-07-27 09:58:20 -0400

    feature: statement with returning

--- examples/sqlite/databaseClient_example_sqlite.py
+++ examples/sqlite/databaseClient_example_sqlite.py
@@ -17,8 +17,15 @@ def excuteCliente(event={}, context={}):
     mySecrectManagerArn : str = "" #set as environment
     databaseManager : DatabaseWrapperManager = createDatabaseManager(myRegion, mySecrectManagerArn)
     databaseManager.executeOneStatement("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
-    databaseManager.executeOneStatement("INSERT INTO users (name) VALUES ('mario')")
-    rows : list[dict[str,object]] = databaseManager.executeQuery("SELECT * FROM users")
+    rows : list[dict[str,object]] = databaseManager.executeStatmentWithReturn("INSERT INTO users (name) VALUES ('mario') RETURNING *")
+    
+    print("before")
+    for r in rows:
+        print(r)
+    
+    print("")
+    print("after")
+    rows = databaseManager.executeQuery("SELECT * FROM users")
     for r in rows:
         print(r)
     databaseManager.closeConnection()
--- src/DatabaseWrapperManager.py
+++ src/DatabaseWrapperManager.py
@@ -8,7 +8,8 @@ class DatabaseWrapperManager(IDatabaseManager):
 
     def __init__(self,dbConnection):
         super().__init__(dbConnection)
-        dbConnection.autocommit = False
+        if(hasattr(dbConnection,"autocommit")):
+            dbConnection.autocommit = False
         self._transactionInProgress = []
         
     def executeQuery(self,queryString:str,params:tuple=None) -> list[dict[str,object]]:
@@ -41,7 +42,18 @@ class DatabaseWrapperManager(IDatabaseManager):
         self.connection.commit()
         cursor.close()
         del cursor
+    
+    def executeStatmentWithReturn(self,queryString:str,params:tuple=None) -> list[dict[str,object]]:
+        
+        self._raiseOnTransactionInProgress()
 
+        cursor = self.connection.cursor()
+        result = SqlCommandExecutor.executeQuery(cursor,queryString,params)
+        self.connection.commit()
+        cursor.close()
+        del cursor
+        return result
+    
     def startTransaction(self) -> TransactionWrapper:
         id = datetime.now().strftime('%Y%m%d%H%M%S%f')
         self._transactionInProgress.append(id)
